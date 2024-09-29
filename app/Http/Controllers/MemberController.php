d1e6484fdd1a00f0e6b6c12f63db8b1d59490fa2
RiTa-23 <rita.tifrt@gmail.com>
2024-09-30 00:24:39 +0900

    メンバー削除機能

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -90,5 +90,7 @@ class MemberController extends Controller
     public function destroy(Member $member)
     {
         //
+        $member->delete();
+        return redirect()->route('members.index');
     }
 }
--- resources/views/members/edit.blade.php
+++ resources/views/members/edit.blade.php
@@ -1,5 +1,3 @@
-<!-- resources/views/tweets/edit.blade.php -->
-
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -12,27 +10,45 @@
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
           <a href="{{ route('members.index', $member) }}" class="text-blue-500 hover:text-blue-700 mr-2">メンバーリストに戻る</a>
+
+          <!-- Update Form -->
           <form method="POST" action="{{ route('members.update', $member) }}">
             @csrf
             @method('PUT')
             <div class="mb-4">
               <label for="studentID" class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2">学籍番号</label>
-              <input type="text" name="studentID" id="studentID" value="{{$member->studentID}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <input type="text" name="studentID" id="studentID" value="{{$member->studentID}}"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
               <label for="name" class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2">名前</label>
-              <input type="text" name="name" id="name" value="{{$member->name}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <input type="text" name="name" id="name" value="{{$member->name}}"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
               <label for="nickname" class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2">ニックネーム</label>
-              <input type="text" name="nickname" id="nickname" value="{{$member->nickname}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <input type="text" name="nickname" id="nickname" value="{{$member->nickname}}"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
               <label for="faculty" class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2">学部</label>
-              <input type="text" name="faculty" id="faculty" value="{{$member->faculty}}"class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <input type="text" name="faculty" id="faculty" value="{{$member->faculty}}"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
               <label for="department" class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2">学科</label>
-              <input type="text" name="department" id="department" value="{{$member->department}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <input type="text" name="department" id="department" value="{{$member->department}}"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
               <label for="grade" class="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2">学年</label>
-              <input type="number" name="grade" id="grade" value="{{$member->grade}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+              <input type="number" name="grade" id="grade" value="{{$member->grade}}"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline">
               @error('member')
-                <span class="text-red-500 text-xs italic">{{ $message }}</span>
+              <span class="text-red-500 text-xs italic">{{ $message }}</span>
               @enderror
             </div>
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Update</button>
+            <button type="submit"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Update</button>
+          </form>
+
+          <!-- Delete Form -->
+          <form method="POST" action="{{ route('members.destroy', $member) }}">
+            @csrf
+            @method('DELETE')
+            <div align="right">
+              <button type="submit" class="text-red-500 hover:text-red-700">メンバー削除</button>
+            </div>
           </form>
         </div>
       </div>
